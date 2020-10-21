52340d4323f52ae9ce61effdc9caa13bdfddf8fd
Syed Zawwar Ahmed <zawwar.ahmed12@gmail.com>
2020-10-21 17:59:15 +0500

    Updates

---
+++ precache-manifest.dd9d7486a613a6efcda4625b488408f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2daec8ed6e7d51b582f54b75c61cc42f",
+    "url": "/Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "7d2f7362864e27239955",
+    "url": "/Markdown-Previewer/static/css/main.50a15b2c.chunk.css"
+  },
+  {
+    "revision": "2f6a81ac260d263f7d30",
+    "url": "/Markdown-Previewer/static/js/2.f0d7502e.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/Markdown-Previewer/static/js/2.f0d7502e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d2f7362864e27239955",
+    "url": "/Markdown-Previewer/static/js/main.8140bbc3.chunk.js"
+  },
+  {
+    "revision": "b0a69d01708cd88e3db1",
+    "url": "/Markdown-Previewer/static/js/runtime-main.baa23fdd.js"
+  }
+]);
